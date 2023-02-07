9e3332a30791fee37d9e562ffdefa75f6de58871
AR-cogoport <atul.raj@coogoport.com>
2023-02-07 17:38:00 +0530

    Completed

--- Substring.rb
+++ Substring.rb
@@ -1,19 +1,16 @@
 
 def solve(s,mp)
-    s1=Hash.new(0)
+    s1=Hash.new
         mp.each do |word|
             if s.downcase.include?(word)
-                if(s.scan(word).size!=0)
-                s1[word]=s.scan(word).size
-                end
+                s1[word]=s.downcase.scan(word).size
          end
      end
-     return s1
+     puts(s1)
  end
 
-s="Howdy partner, sit down! How's it going?"
-m = ["below","below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+s=gets.chomp
+ m = ["below","below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
     
-puts(solve("below",m))
-puts(solve("Howdy partner, sit down! How's it going?",m))
+solve(s,m)
